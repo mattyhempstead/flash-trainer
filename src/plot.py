9872b5f6a6c54be5dbbc1b06a53b06cc039e23d6
mattyhempstead <matty.hempstead@gmail.com>
2022-07-05 15:41:41 +1000

    Add groups session line plot

--- src/plot.py
+++ src/plot.py
@@ -20,6 +20,29 @@ def boxplot_q_group(group_col:str):
     plt.show()
 
 
+def line_q_group(group_col:str):
+    """ Plots lines per session """
+    df_q = results.df_q
+
+    df_q_group = df_q.groupby([group_col,"s_num"])
+    df_q_group = pd.DataFrame(df_q_group["q_time_total"].mean())
+    df_q_group = df_q_group.reset_index(drop=False)
+    print(df_q_group)
+
+    import seaborn as sns
+
+    sns.lineplot(
+        x="s_num", y="q_time_total", data=df_q_group,
+        hue=group_col, palette="tab10", legend=False,
+    )
+    
+    sns.scatterplot(
+        x="s_num", y="q_time_total", data=df_q_group,
+        hue=group_col, palette="tab10", legend=True,
+    )
+
+    plt.show()
+
 
 def plot_q_num():
     q_ans_y = np.array(list(results.df_results.groupby(['s_num'])["q_num"].max() - 1))
@@ -41,12 +64,15 @@ results = Results("results_ttables")
 
 # results.df_q_operand_time
 
+line_q_group("q_operand_1")
+
+
 # boxplot_q_group("q_operator")
 # boxplot_q_group("q_operand_1")
 # boxplot_q_group("q_operand_2")
 # boxplot_q_group("s_num")
 # boxplot_q_group("q_answer")
-boxplot_q_group("q_num")
+# boxplot_q_group("q_num")
 
 # plot_q_num()
 
